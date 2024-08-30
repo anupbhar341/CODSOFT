7d22739cdb834e7130b76f1750e9bdca2c6b2d78
anupbhar341 <anupbhar341@gmail.com>
2024-08-31 03:04:34 +0530

    Added two new projects

---
+++ Portfolio1/script.js
@@ -0,0 +1,6 @@
+let type = new Typed('#changing-text',{
+    strings: ["Front-end developer", "GATE Qualified", "Proficient C/C++ Coder"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+});
