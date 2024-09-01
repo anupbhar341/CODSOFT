dc4dc9f7b4f46d8c135e1a1408259628cfceb978
Anup Bhar <145141774+anupbhar341@users.noreply.github.com>
2024-09-01 16:22:19 +0530

    Delete Portfolio1 directory

--- Portfolio1/script.js
+++
@@ -1,6 +0,0 @@
-let type = new Typed('#changing-text',{
-    strings: ["Front-end developer", "GATE Qualified", "Proficient C/C++ Coder"],
-    typeSpeed: 50,
-    backSpeed: 50,
-    loop: true,
-});
